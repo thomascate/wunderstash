7a9c29fb847cdcf245b7308747f12dfbdf7fc9cf
Thomas Cate <thomascate@demandmedia.com>
2014-12-20 15:52:27 -0600

    added cleanup of data to make sure numbers are inserted correctly

--- wunderground.py
+++ wunderground.py
@@ -30,17 +30,57 @@ esObject = {
             '_source': currentWeather
            }
 
-if esObject['_source']['current_observation']['local_epoch']:
+if esObject['_source']['current_observation']['observation_epoch']:
   esObject['_source']['@timestamp'] = datetime.utcfromtimestamp(float(esObject['_source']['current_observation']['local_epoch']))
 else:
   esObject['_source']['@timestamp'] = datetime.utcnow()
+
+#clean up data since Wunderground responds with unicode numbers occasionally
+#this sucks
+floats = [
+          'dewpoint_c',
+          'dewpoint_f',
+          'feelslike_c',
+          'feelslike_f',
+          'heat_index_c',
+          'heat_index_f',
+          'observation_location',
+          'observation_location',
+          'precip_1hr_in',
+          'precip_1hr_metric',
+          'precip_today_in',
+          'precip_today_metric',
+          'pressure_mb',
+          'temp_c' ,
+          'temp_f',
+          'visibility_km',
+          'wind_degrees',
+          'wind_gust_kph',
+          'wind_gust_mph',
+          'wind_kph',
+          'wind_mph',
+          'UV',
+          'visibility_mi',
+]
+
+for entry in floats:
+ try:
+   esObject['_source']['current_observation'][entry] = float(esObject['_source']['current_observation'][entry])
+ except:
+   pass
+
+try:
+  esObject['_source']['current_observation']['observation_location']['longitude'] = float(esObject['_source']['current_observation']['observation_location']['longitude'])
+  esObject['_source']['current_observation']['observation_location']['latitude']  = float(esObject['_source']['current_observation']['observation_location']['latitude'])
+except:
+  pass
 
 #making a bulkObject list so it will be easier to do multiple cities later on
 bulkObject = []
 bulkObject.append(esObject)
 
 es = Elasticsearch([esHost], sniff_on_start=True)
-es.indices.create(index=esIndex, body = esIndexSettings, ignore=400)
+es.indices.create(index=esIndex, body=esIndexSettings, ignore=400)
 
 if len(bulkObject) > 0:
   helpers.bulk(es, bulkObject)
